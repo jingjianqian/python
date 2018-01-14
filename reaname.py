3a9acf92845d44d91919baf9f8772fa73ac063d6
jingjianqian <18697998680@163.com>
2018-01-14 19:36:57 +0800

    Delete reaname.py

--- reaname.py
+++
@@ -1,7 +0,0 @@
-import os
-path="H:\\work\\everyday\\2015-DQTEST"
-path_all = os.listdir(path)
-for path_all_obj in range(0,len(path_all)):
-	this_path = os.path.join(path,path_all[path_all_obj])
-	if os.path.isfile(this_path):
-		os.rename(this_path,this_path.lower())
