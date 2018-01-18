2f2e5b78c8323895d59b4d9bfdfd0400c7e1aadd
jingjianqian <18697998680@163.com>
2018-01-18 15:18:16 +0800

    删掉没有用的文件

--- script/get_pdf_file.py
+++ script/get_pdf_file.py
@@ -6,8 +6,6 @@ import random
 
 def getPdfs(filepath):
 	print('开始查找:'+filepath+' 下的pdf文件,准备好没有!?狗蛋')
-	a = random.randint(1,1000)
-	print(a)
 	rootObjs = os.listdir(filepath)
 	for rootObj in rootObjs:
 		rootObjAbsPath = os.path.join(filepath,rootObj)
@@ -17,19 +15,21 @@ def getPdfs(filepath):
 			(shortname,extension) = os.path.splitext(rootObj)
 			#print(extension.upper())
 			if extension.upper() == '.PDF':
-				pass
 				#shutil.copyfile(os.path.join(filepath,rootObj),os.path.join('H:\\room\\py\\tool\\pdfs',rootObj.upper()))
 				os.rename(os.path.join(filepath,rootObj),os.path.join(filepath,rootObj.upper()))
+				print(os.path.join(filepath,rootObj)+'------->'+rootObj.upper())
 				#print(shortname+'.'+extension)
 			else:
 				try:
-
-					rang_num1 = random.randint(1,1000)
-					rang_num2 = random.randint(1,1000)
-					rang_num3 = random.randint(1,1000)
-					shutil.move(os.path.join(filepath,rootObj),os.path.join('X:\\notpdf\\1',rootObj))
+					os.remove(os.path.join(filepath,rootObj))
+					#shutil.move(os.path.join(filepath,rootObj),os.path.join('X:\\notpdf\\1',rootObj))
+					print('delete:'+os.path.join(filepath,rootObj))
 				except Exception as e:
-					rang_num = random.randint(1,1000)
-					shutil.move(os.path.join(filepath,rootObj),os.path.join('X:\\notpdf\\1','1_'+str(rang_num)+rootObj))
+					pass
+					#os.remove(os.path.join(filepath,rootObj))
+					#rang_num = random.randint(1,1000)
+					#shutil.move(os.path.join(filepath,rootObj),os.path.join('X:\\notpdf\\1','1_'+str(rang_num)+rootObj))
+					#print('move:'+os.path.join(filepath,rootObj))
 
-getPdfs(u'X:\\search')
+print('start.....')
+getPdfs(u'X:\\search\\原文2013以后\\jx')
--- script/oracle_con.py
+++ script/oracle_con.py
@@ -1,5 +1,5 @@
 import cx_Oracle
-conn = cx_Oracle.connect('system/orcl@127.0.0.1/ora11g')
+conn = cx_Oracle.connect('system/orcl@127.0.0.1/orcl')
 c = conn.cursor()
 x = c.execute('select  sysdate from dual')
 x.fetchone()
